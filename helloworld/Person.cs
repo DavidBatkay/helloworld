a529562ed23de21dde236e03e85d82f30130f30f
DavidDVB <batkaydavid2003@gmail.com>
2023-10-02 09:45:26 +0300

    push

---
+++ helloworld/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    internal class Person
+    {
+        public string Name="Emanuel";
+
+
+
+        public void ReadName()
+        {
+            Console.Write("Your name is= ");
+            Name = Console.ReadLine();
+        }
+
+
+        public void SayHello()
+
+        {
+            Console.WriteLine("Hello, my name is " + Name);
+        }
+    }
+}
--- helloworld/Program.cs
+++ helloworld/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace HelloWorld
+﻿using helloworld;
+
+namespace HelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            //int i = 10;
+            //Console.WriteLine(i);
+
+            Person p1= new Person();
+            p1.ReadName();
+            p1.SayHello();
+
         }
     }
 }
